0944441c6a55122b1154c03ec54c74c2f492a1f0
Hez Ronningen <code@hez.ca>
2020-02-21 11:51:11 -0800

    Update erlang LD lib
    - rename all calls to new module namespace

--- lib/ex_launch_darkly/app.ex
+++ lib/ex_launch_darkly/app.ex
@@ -1,11 +1,12 @@
 defmodule ExLaunchDarkly.App do
   @spec start(String.t()) :: :ok | {:error, any(), any()}
-  def start(key) when is_binary(key), do: key |> String.to_charlist() |> :eld.start_instance()
+  def start(key) when is_binary(key),
+    do: key |> String.to_charlist() |> :ldclient.start_instance()
 
   @spec start(String.t(), atom()) :: :ok | {:error, any(), any()}
   def start(key, tag) when is_binary(key) and is_atom(tag),
-    do: key |> String.to_charlist() |> :eld.start_instance(tag)
+    do: key |> String.to_charlist() |> :ldclient.start_instance(tag)
 
   @spec stop_all() :: :ok
-  def stop_all, do: :eld.stop_all_instances()
+  def stop_all, do: :ldclient.stop_all_instances()
 end
