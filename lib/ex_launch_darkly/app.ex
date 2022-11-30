a0bc8ef0a2613b3dd480fa3c2d1ce31adf1a8cba
Graham Baradoy <graham@baradoy.ca>
2022-11-30 09:50:37 -0800

    Allow map as paramater to App.start/2
    This matches https://hexdocs.pm/launchdarkly_server_sdk/ldclient.html#start_instance-2

--- lib/ex_launch_darkly/app.ex
+++ lib/ex_launch_darkly/app.ex
@@ -3,9 +3,9 @@ defmodule ExLaunchDarkly.App do
   def start(key) when is_binary(key),
     do: key |> String.to_charlist() |> :ldclient.start_instance()
 
-  @spec start(String.t(), atom()) :: :ok | {:error, any(), any()}
-  def start(key, tag) when is_binary(key) and is_atom(tag),
-    do: key |> String.to_charlist() |> :ldclient.start_instance(tag)
+  @spec start(String.t(), atom() | map()) :: :ok | {:error, any(), any()}
+  def start(key, tag_or_map) when is_binary(key) and (is_atom(tag_or_map) or is_map(tag_or_map)),
+    do: key |> String.to_charlist() |> :ldclient.start_instance(tag_or_map)
 
   @spec stop_all() :: :ok
   def stop_all, do: :ldclient.stop_all_instances()
